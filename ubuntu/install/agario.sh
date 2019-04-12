9d8587ef16fffebfb28e89879bbf5b179853a0e8
Nick Wylynko <nick1014375@gmail.com>
2019-04-12 19:53:51 -0400

    adding name for agario

--- ubuntu/install/agario.sh
+++ ubuntu/install/agario.sh
@@ -6,5 +6,5 @@ rm master.zip
 mv agar.io-clone-master agario
 cd agario
 docker build -t agarioclone_agar .
-docker create -it -p 3000:3000 agario
+docker create -it -p 3000:3000 --name agario agarioclone_agar
 docker start agario
