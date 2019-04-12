99e35cdd83b1598ced0246e7dd81e64905c16110
Nick Wylynko <nick1014375@gmail.com>
2019-04-12 18:03:49 -0400

    Update agario.sh

--- ubuntu/install/agario.sh
+++ ubuntu/install/agario.sh
@@ -2,7 +2,9 @@
 apt install unzip -y
 wget https://github.com/huytd/agar.io-clone/archive/master.zip
 unzip master.zip
+rm master.zip
 mv agar.io-clone-master agario
 cd agario
 docker build -t agarioclone_agar .
-docker run -it -p 3000:3000 agarioclone_agar
+docker create -it -p 3000:3000 agario
+docker start agario
