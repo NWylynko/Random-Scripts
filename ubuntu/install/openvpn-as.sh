e1b32cd7c1116a200071c4dd4eee357b702228cd
Nick Wylynko <nick1014375@gmail.com>
2019-04-15 19:52:49 -0400

    Create openvpn-as.sh

---
+++ ubuntu/install/openvpn-as.sh
@@ -0,0 +1,13 @@
+docker volume create --name openvpn-data
+docker create \
+  --name=openvpn-as \
+  --cap-add=NET_ADMIN \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=America/Virginia \
+  -p 943:943 \
+  -p 9443:9443 \
+  -p 1194:1194/udp \
+  -v openvpn-data:/config \
+  --restart unless-stopped \
+  linuxserver/openvpn-as
