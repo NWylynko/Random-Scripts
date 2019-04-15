b6e71c9d58e40d76052005582d3c924f5d6f2051
Nick Wylynko <nick1014375@gmail.com>
2019-04-15 19:35:16 -0400

    Update agario.sh

--- ubuntu/reinstall/agario.sh
+++ ubuntu/reinstall/agario.sh
@@ -2,7 +2,8 @@
 docker stop agario
 docker rm agario
 
-git fetch https://github.com/NWylynko/agar.io-clone.git /root/home/agario
+rm -R /root/home/agario
+git clone https://github.com/NWylynko/agar.io-clone.git /root/home/agario
 
 docker build -t agarioclone_agar /root/home/agario
 docker create -it -p 3000:3000 --name agario agarioclone_agar
