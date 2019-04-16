4874c27e7e1195f246474ce3512bbd4e3aa84a52
Nick Wylynko <nick1014375@gmail.com>
2019-04-16 09:53:09 -0400

    auto yes

--- centos/install/docker.sh
+++ centos/install/docker.sh
@@ -6,12 +6,12 @@ yum remove docker \
                   docker-latest \
                   docker-latest-logrotate \
                   docker-logrotate \
-                  docker-engine
+                  docker-engine -y
 
 yum install -y yum-utils \
 device-mapper-persistent-data \
 lvm2
 yum-config-manager \
     --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io
+    https://download.docker.com/linux/centos/docker-ce.repo -y
+yum install docker-ce docker-ce-cli containerd.io -y
