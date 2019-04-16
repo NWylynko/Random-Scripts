685e15998dca3c26e4a71699a0ab689725c6d112
Nick Wylynko <nick1014375@gmail.com>
2019-04-16 16:02:24 -0400

    Update openvpn.sh

--- docker/install/openvpn.sh
+++ docker/install/openvpn.sh
@@ -5,6 +5,6 @@ OVPN_DATA="openvpn-data"
 docker volume create --name $OVPN_DATA
 docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u udp://$IP:1194
 docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
-docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
+docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:443/udp --cap-add=NET_ADMIN kylemanna/openvpn
 docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full STCVA nopass
 docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient STCVA > STCVA.ovpn
