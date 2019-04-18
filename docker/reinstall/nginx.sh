4af1272641d99b247b8ae2b80e2ac759f05f6256
Nick Wylynko <nick1014375@gmail.com>
2019-04-18 09:52:33 -0400

    Update nginx.sh

--- docker/reinstall/nginx.sh
+++ docker/reinstall/nginx.sh
@@ -3,5 +3,6 @@ docker stop test-website_nginx_1
 docker rm test-website_nginx_1
 rm -R my-dockers
 git clone https://github.com/NWylynko/my-dockers.git
-chmod +x my-docker/test-website/init-letsencrypt.sh
-./my-docker/test-website/init-letsencrypt.sh
+cd my-dockers/test-website/
+chmod +x init-letsencrypt.sh
+./init-letsencrypt.sh
